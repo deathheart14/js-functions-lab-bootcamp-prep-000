eea242ab3c3415aaf2a36fdda94a549285b5fb9d
Jason Barnes <deathheart14@yahoo.com>
2017-09-09 01:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string) {
-  var name = string
+function happyHolidaysTo(name) {
+  var name = name
   var string1 = 'Happy holidays, ${name}'
+  return string1
+}
+
+function happyHolidaysTo(holiday, name) {
+  var name = name
+  var holiday = holiday
+  var string1 = 'Happy ${holiday}, ${name}'
   return string1
 }
