bffd97edad7ff3403cb9e995f39082063c0260be
Jason Barnes <deathheart14@yahoo.com>
2017-09-09 01:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  var name = name
-  var string1 = 'Happy holidays, ${name}'
-  return string1
+
 }
 
 function happyHolidayTo(holiday, name) {
-  var name = name
-  var holiday = holiday
-  var string1 = 'Happy ${holiday}, ${name}'
-  return string1
+
 }
 
 function holidayCountdown(holiday, days) {
 
 
-  return 'It\'s' ${days} 'days until' ${holiday}
+
 }
