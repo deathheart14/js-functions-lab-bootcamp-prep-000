e69f68f2e6c60fd4f2e60679bd33465778b5cb39
Jason Barnes <deathheart14@yahoo.com>
2017-09-09 01:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(string) {
+  var name = string
+  var string1 = 'Happy holidays, ${name}'
+  return string1
+}
